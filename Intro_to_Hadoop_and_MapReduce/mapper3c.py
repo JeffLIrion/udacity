b6315d4a9cd68bbdd9f2b4602b62b9fdcf0c2cdc
JeffLIrion <jlirion@ucdavis.edu>
2016-03-10 05:27:00 -0500

    Finished problem 3, part 1

--- Intro_to_Hadoop_and_MapReduce/mapper3c.py
+++ Intro_to_Hadoop_and_MapReduce/mapper3c.py
@@ -3,7 +3,7 @@
 # Format of each line is:
 # date\ttime\tstore name\titem description\tcost\tmethod of payment
 #
-# We want elements 3 (item description) and 4 (cost)
+# We want elements 2 (store name) and 4 (cost)
 # We need to write them out to standard output, separated by a tab
 
 import sys
@@ -11,5 +11,5 @@ import sys
 for line in sys.stdin:
     data = line.strip().split("\t")
     if len(data) == 6:
-        print "{0}\t{1}".format(data[3], data[4])
+        print "{0}\t{1}".format(data[2], data[4])
 
